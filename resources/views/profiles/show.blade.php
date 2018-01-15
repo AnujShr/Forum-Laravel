a0d9ec6b65cec93269091eb4e5e01adead1b0134
Anuj Shrestha <Anuj.me3@gmail.com>
2018-01-16 01:33:58 +0545

    group activity feed by date

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -13,6 +13,7 @@
                     </div>
                 </div>
                 @foreach($activities as $date=>$activity)
+                    <h3 class="page-header">{{$date}}</h3>
                     @foreach($activity as $record)
                         @include("profiles.activities.{$record->type}",['activity'=>$record])
                     @endforeach
